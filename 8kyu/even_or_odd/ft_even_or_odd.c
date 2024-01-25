6b050fee6b6744098ae7f129ecf3294ddd7831b4
Lehendss <edisonfcm1@gmail.com>
2024-01-25 17:45:45 +0000

    Added my first exercise :)

---
+++ 8kyu/even_or_odd/ft_even_or_odd.c
@@ -0,0 +1,13 @@
+const   char* even_or_odd(int number)
+{
+    if (number % 2 == 0)
+        return("Even");
+    return ("Odd");
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("%s\n", even_or_odd(-3));
+}*/
+
